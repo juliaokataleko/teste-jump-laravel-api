121f791d60dc5483af9551e2883a63ff807fa2a5
Julião Kataleko <juliofeli78@gmail.com>
2023-05-24 08:56:17 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory()
+            ->count(25)
+            ->hasServiceOrders(20)
+            ->create();
+
+        User::factory()
+            ->count(20)
+            ->hasServiceOrders(10)
+            ->create();
+
+        User::factory()
+            ->count(10)
+            ->hasServiceOrders(5)
+            ->create();
+    }
+}
