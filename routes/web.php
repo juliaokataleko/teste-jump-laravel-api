c21074786c3b1b0e51c9b8b2c84779ce745f386c
Julião Kataleko <juliofeli78@gmail.com>
2023-05-28 13:09:47 +0100

    readme and app auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,47 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/setup", function () {
+
+    $credencials = [
+        "email" => "admin@admin.com",
+        "password" => "password"
+    ];
+
+    if (!Auth::attempt($credencials)) {
+        $user = User::create([
+            "email" => "admin@admin.com",
+            "name" => "Admin",
+            "password" => bcrypt("password")
+        ]);
+
+        if (Auth::attempt($credencials)) {
+            
+            $user = Auth::user();
+            $adminToken = $user->createToken("admin-token", ["create", "update", "delete"]);
+            $updateToken = $user->createToken("update-token", ["create", "update"]);
+            $basicToken = $user->createToken("update-token");
+
+            return [
+                "admin" => $adminToken->plainTextToken,
+                "update" => $updateToken->plainTextToken,
+                "basic" => $basicToken->plainTextToken
+            ];
+
+        }
+    } else {
+        $user = Auth::user();
+        $adminToken = $user->createToken("admin-token", ["create", "update", "delete"]);
+        $updateToken = $user->createToken("update-token", ["create", "update"]);
+        $basicToken = $user->createToken("update-token");
+
+        return [
+            "admin" => $adminToken->plainTextToken,
+            "update" => $updateToken->plainTextToken,
+            "basic" => $basicToken->plainTextToken
+        ];
+    }
+
 });
